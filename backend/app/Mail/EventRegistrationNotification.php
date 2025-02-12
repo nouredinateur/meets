1936030b611d6c640046404d0508f5786fb2f43a
nouredinateur <nafzaouinourdine@gmail.com>
2025-02-12 18:52:41 +0100

    EventRegistration mailing

---
+++ backend/app/Mail/EventRegistrationNotification.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Event;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class EventRegistrationNotification extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $event;
+    public $user;
+
+    public function __construct(Event $event, User $user)
+    {
+        $this->event = $event;
+        $this->user = $user;
+    }
+
+    public function build()
+    {
+        return $this->subject('New Event Registration')
+            ->view('vendor/mail/html/event-registration')
+            ->with([
+                'eventTitle' => $this->event->title,
+                'userName' => $this->user->name,
+                'eventDate' => $this->event->date,
+            ]);
+    }
+}
---
+++ backend/resources/views/vendor/mail/html/event-registration.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>New Registration</title>
+</head>
+
+<body>
+    <h1>New Registration for Your Event</h1>
+    <p>Hello,</p>
+    <p>{{ $userName }} has registered for your event "{{ $eventTitle }}" scheduled on {{ $eventDate }}.</p>
+    <p>Login to your account to view details.</p>
+</body>
+
+</html>

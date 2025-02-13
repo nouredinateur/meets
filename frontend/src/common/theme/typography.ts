2f4c426b5f55a2b113356eb83cafc58a16501ac3
nouredinateur <nafzaouinourdine@gmail.com>
2025-02-13 12:55:00 +0100

    change font

--- frontend/src/common/theme/typography.ts
+++ frontend/src/common/theme/typography.ts
@@ -1,12 +1,12 @@
 import { Palette } from '@mui/material';
 import { TypographyOptions } from '@mui/material/styles/createTypography';
 // eslint-disable-next-line camelcase
-import { Public_Sans } from '@next/font/google';
+import { Inter } from '@next/font/google';
 
-const publicSans = Public_Sans({
+const publicSans = Inter({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
   subsets: ['latin'],
-  style: ['normal', 'italic'],
+  style: ['normal'],
   display: 'swap',
 });
 

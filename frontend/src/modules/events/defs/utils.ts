27e421004147ce7c8656028b7ef0a7e8ef086490
nouredinateur <nafzaouinourdine@gmail.com>
2025-02-13 11:11:40 +0100

    add date formatting utils

---
+++ frontend/src/modules/events/defs/utils.ts
@@ -0,0 +1,17 @@
+export const formatDate = (dateString: string) => {
+  const date = new Date(dateString);
+  return date.toLocaleDateString('en-US', {
+    month: 'short',
+    day: 'numeric',
+    year: 'numeric',
+  });
+};
+
+export const formatTime = (dateString: string) => {
+  const date = new Date(dateString);
+  return date.toLocaleTimeString('en-US', {
+    hour: 'numeric',
+    minute: '2-digit',
+    hour12: true,
+  });
+};
